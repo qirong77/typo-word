240be6001261fac092cbb9d1ae390234a6938349
qirong77 <qirong7777@gmail.com>
2025-04-20 17:12:19 +0800

    feat: 功能开发

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { Word } from "./components/Word";
 import { useLearningState } from "./hooks/useLearningStateGroup";
 import { Spin } from "antd";
+import { InputStateBoard } from "./components/InputState";
 // import { Settings } from "./components/Settings";
 export default () => {
-    const { word, eatWord,isLoading } = useLearningState(10);
+    const { word, eatWord, isLoading } = useLearningState(2);
     const [userInputWord, setUserInputWord] = useState("");
     const [showRealWord, setShowRealWord] = useState(false);
+    const [inputState, setInputState] = useState({
+        count: 0,
+        errorCout: 0,
+        timeElapsed: "00:00",
+        accuracy: 0,
+    });
     const errorCountRef = useRef(0);
     useEffect(() => {
+        let timer: any;
         document.addEventListener("keydown", (e) => {
             if (e.key === "Backspace") {
                 setUserInputWord((v) => v.slice(0, v.length - 1));
@@ -19,7 +27,22 @@ export default () => {
                 e.preventDefault();
                 return;
             }
+            if (!timer) {
+                timer = setInterval(() => {
+                    console.log('timer');
+                    setInputState((v) => {
+                        return {
+                            ...v,
+                            timeElapsed:'xx:xx'
+                        };
+                    });
+                }, 1000);
+            }
+            setInputState((v) => ({ ...v, count: v.count + 1, accuracy: Math.floor(((v.count - v.errorCout) / v.count) * 100) }));
             setUserInputWord((v) => v + e.key);
+            return () => {
+                clearInterval(timer);
+            };
         });
     }, []);
     useEffect(() => {
@@ -33,15 +56,17 @@ export default () => {
         }
         if (!word.word.includes(userInputWord)) {
             errorCountRef.current++;
+            setInputState((v) => ({ ...v, errorCout: v.errorCout + 1 }));
             if (errorCountRef.current === 3) {
                 setShowRealWord(true);
             }
         }
-    }, [userInputWord, word]);
+    }, []);
     return (
-        <div className="w-screen h-screen flex justify-center items-center bg-slate-900">
-            {isLoading && <Spin size="large"/>}
+        <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-900">
+            {isLoading && <Spin size="large" />}
             {!isLoading && word && <Word showRealWord={showRealWord} word={word} userInputWord={userInputWord} />}
+            <InputStateBoard {...inputState} />
             {/* <Settings /> */}
         </div>
     );
---
+++ src/components/InputState.tsx
@@ -0,0 +1,26 @@
+type StatItemProps = {
+    label: string;
+    value: string | number;
+};
+
+function StatItem({ label, value }: StatItemProps) {
+    return (
+        <div className=" flex justify-center items-center flex-col" style={{
+            padding:'10px 30px',
+        }}>
+            <div className="text-xl  text-slate-500">{value}</div>
+            <div className=" text-slate-500">{label}</div>
+        </div>
+    );
+}
+
+export function InputStateBoard(props: { count: number; errorCout: number; timeElapsed: string; accuracy: number }) {
+    return (
+        <div className=" flex justify-around  bg-slate-800 rounded-lg " style={{marginBottom:'100px'}}>
+            <StatItem label="时间" value={props.timeElapsed} />
+            <StatItem label="输入数" value={props.count} />
+            <StatItem label="正确数" value={props.count} />
+            <StatItem label="正确率" value={`${props.accuracy}%`} />
+        </div>
+    );
+}
