3e8907b1f12b32ebce9ef7a0d29b6647bf448fb0
qirong77 <qirong7777@gmail.com>
2025-04-23 14:16:15 +0800

    修复了一些 Bug，提升了性能，看了许多猫咪视频。 🐱

--- src/components/ToolBar/Settings.tsx
+++ src/components/ToolBar/Settings.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export function Settings() {
     const [visible, setVisible] = useState(false);
 
-    const content = <div>todo...</div>;
+    const content = <div>开发中...</div>;
     return (
         <Popover content={content} title={"设置"} trigger="click" open={visible} onOpenChange={setVisible} placement="bottomRight" arrow={true}>
             <Button icon={<SettingOutlined />}>设置</Button>
