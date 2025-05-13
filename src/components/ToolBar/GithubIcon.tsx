44cfa109e8d3986e90d736667bb3021a4ffcb64a
qirong77 <qirong7777@gmail.com>
2025-05-13 20:10:26 +0800

    修复了一些 Bug，提升了性能，喝了太多茶。🍵

--- src/components/ToolBar/GithubIcon.tsx
+++ src/components/ToolBar/GithubIcon.tsx
@@ -1,7 +1,8 @@
+import GithubImage from "../../../public/assets/github-mark-white.png";
 export function GithubIcon() {
     return (
         <div onClick={() => window.open("https://github.com/qirong77/typo-word", "_blank")}>
-            <img src="../../../public/assets/github-mark-white.png" alt="github" className="w-8 h-8 cursor-pointer opacity-50" />
+            <img src={GithubImage} alt="github" className="w-8 h-8 cursor-pointer opacity-50" />
         </div>
     );
 }
